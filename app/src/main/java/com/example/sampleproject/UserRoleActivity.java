d14fa3a379344cdfb572ed26cffb1c0a4cd28cc1
DangHung <danghungabt.bte@gmail.com>
2023-11-28 13:40:06 +0700

    add activity user-role

---
+++ app/src/main/java/com/example/sampleproject/UserRoleActivity.java
@@ -0,0 +1,14 @@
+package com.example.sampleproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserRoleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_role);
+    }
+}
